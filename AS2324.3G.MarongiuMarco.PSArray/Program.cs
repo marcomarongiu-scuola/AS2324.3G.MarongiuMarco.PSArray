e62b36638df9aadbec246229e77b7050fa01def7
marko marongiu <coentro07@gmail.com>
2024-02-29 08:37:28 +0100

    feat: Implementare un funzione StampaVotiPesi per la stampa per righe distinte del voto e del corrispondente peso

--- AS2324.3G.MarongiuMarco.PSArray/Program.cs
+++ AS2324.3G.MarongiuMarco.PSArray/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("29/02/2024 Marongiu Marco 3g prova singola sugli array ");
+            Console.WriteLine("Quanti voti vuoi immettere?");
+            int nVoti=Convert.ToInt32(Console.ReadLine());
+            double[] voti = new double[nVoti];
+            int[] pesi = new int[nVoti];
+
+        }
+        void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            Console.WriteLine("Stampa dei voti e dei pesi");
+            foreach (int v in voti)
+            {
+                Console.Write("Voti: " + voti[v]);
+                Console.WriteLine("Pesi: "+ pesi[v]);
+            }
         }
     }
 }
