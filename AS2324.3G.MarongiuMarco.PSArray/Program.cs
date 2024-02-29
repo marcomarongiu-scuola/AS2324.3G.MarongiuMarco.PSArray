efd36eb72cb9f2a54d39fbae6ba316093cff785c
marko marongiu <coentro07@gmail.com>
2024-02-29 08:43:25 +0100

    feat: carica dei due vettori con valori casuali

--- AS2324.3G.MarongiuMarco.PSArray/Program.cs
+++ AS2324.3G.MarongiuMarco.PSArray/Program.cs
@@ -9,15 +9,25 @@
             int nVoti=Convert.ToInt32(Console.ReadLine());
             double[] voti = new double[nVoti];
             int[] pesi = new int[nVoti];
-
+            CaricaVettori(ref voti, ref pesi);
+            StampaVotiPesi(voti, pesi);
         }
-        void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        static void StampaVotiPesi(double[] voti, int[] pesi)
         {
             Console.WriteLine("Stampa dei voti e dei pesi");
             foreach (int v in voti)
             {
                 Console.Write("Voti: " + voti[v]);
                 Console.WriteLine("Pesi: "+ pesi[v]);
+            }
+        }
+        static void CaricaVettori(ref double[] voti, ref int[] pesi)
+        {
+            Random random = new Random();
+            foreach(int v in voti)
+            {
+                voti[v] = random.Next(1, 11);
+                pesi[v] = random.Next(0, 101);
             }
         }
     }
